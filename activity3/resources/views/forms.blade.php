af994f4dc9d59b6901a72ac4aa83f7204f0ac6d9
Paolo Ignacio <179119080+paolo-ignacio@users.noreply.github.com>
2025-03-06 19:17:51 +0800

    Revert "add activity5"
    This reverts commit 44d1bc8764cdcebc1bb52649f8cf7b57296caaa0.

--- activity3/resources/views/forms.blade.php
+++ activity3/resources/views/forms.blade.php
@@ -7,90 +7,30 @@
 </head>
 <body>
 
-    <h1>Personal Information</h1>
-    @if(session('success'))
-    <p style="color:green">{{session('success')}}</p>
-    @endif
+<form action="" method="post">
+    <div>
+        <label for="firstName">First Name:</label>
+        <input type="text" name="firstName" value="{{firstName}}">
+        <span style="color:red"></span>
+    </div>
+    <div>
+        <label for="lastName">Last Name:</label>
+        <input type="text" name="lastName" value="{{lastName}}">
+        <span style="color:red"></span>
+    </div>
+    <div>
+        <input type="radio" id="male" name="sex" value="male">
+        <label for="male">Male</label>
+        <input type="radio" id="female" name="sex" value="female">
+        <label for="female">Female</label>
+    </div>
+    <div>
+        <label for="mobile">Mobile Phone:</label>
+        <input type="text" name="first_name" value="{{first_name}}">
+        <span style="color:red"></span>
+    </div>
 
-
-
-    @if($errors->any())
-    <ul>
-        @foreach($errors->all() as $error)
-        <li style="color:red">{{$error}}</li>
-        @endforeach
-    </ul>
-    @endif
-    <form action="{{route('forms')}}" method="post">
-    @csrf
-        <div>
-            <label for="firstName">First Name:</label>
-            <input type="text" name="firstName" value="{{old('firstName')}}">
-            @error('firstName') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="lastName">Last Name:</label>
-            <input type="text" name="lastName" value="{{old('lastName')}}">
-            @error('lastName') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="sex">Sex:</label>
-            <input type="radio" id="male" name="sex" value="Male">
-            <label for="male">Male</label>
-            <input type="radio" id="female" name="sex" value="Female">
-            <label for="female">Female</label>
-            @error('sex') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="mobile">Mobile Phone:</label>
-            <input type="text" name="mobile" value="{{old('mobile')}}">
-            @error('mobile') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="telephone">Telephone No:</label>
-            <input type="text" name="telephone" value="{{old('telephone')}}">
-            @error('telephone') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="birth_date">Birth Date:</label>
-            <input type="text" name="birth_date" value="{{old('birth_date')}}">
-            @error('birth_date') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="address">Address:</label>
-            <input type="text" name="address" value="{{old('address')}}">
-            @error('address') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="email">Email:</label>
-            <input type="text" name="email" value="{{old('email')}}">
-            @error('email') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <div>
-            <label for="website">Website:</label>
-            <input type="text" name="website" value="{{old('website')}}">
-            @error('website') 
-                <span style="color:red">{{ $message }}</span> 
-            @enderror
-        </div>
-        <button type="submit">Submit</button>
-    </form>
+</form>
     
 </body>
 </html>
--- activity3/routes/web.php
+++ activity3/routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
-
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\FormController;
+use App\Http\Controllers\OrderController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('forms', [FormController::class, 'showForm'])->name('forms');
-Route::post('forms',[FormController::class, 'handleForm']);
